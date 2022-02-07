102f80c44c7b7b6ad6cb7d0c32ea1ffe22ce66b6
icebenitez <icebenitez@gmail.com>
2022-02-07 18:01:23 +0800

    updated layout jsx fragment

--- components/layout.js
+++ components/layout.js
@@ -2,11 +2,11 @@ import Head from "next/head";
 
 export default function Layout({children}){
     return(
-        <div>
+        <>
             <Head>
                 <title>NextJs Template</title>
             </Head>
             {children}
-        </div>
+        </>
     )
 }
